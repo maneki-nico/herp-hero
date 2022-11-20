d5a5e9b90ad55c4dde9a15f75d38ed77e7c357eb
Nico Foti <nicolefoti@gmail.com>
2022-11-20 12:24:16 -0500

    more fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,29 @@
-user_1 = User.create(
-  email: "email@email.com",
-  password: "mypassword",
-  zip: "02145"
-)
+# Initial seeding is commented out to prevent re-seeding data 
 
-user_2 = User.create(
-  email: "ilovegeckos@geckomail.com",
-  password: "geckolover",
-  zip: "12345"
-)
+# user_1 = User.create(
+#   email: "email@email.com",
+#   password: "mypassword",
+#   zip: "02145"
+# )
 
-pet_1 = Pet.find_or_create_by(
-  name: "Spot",
-  animal: "lizard", 
-  species: "black and white tegu",
-  gender: "male",
-  user: user_1
-)
+# user_2 = User.create(
+#   email: "ilovegeckos@geckomail.com",
+#   password: "geckolover",
+#   zip: "12345"
+# )
 
-pet_2 = Pet.find_or_create_by(
-  name: "Pearl",
-  animal: "lizard",
-  species: "leopard gecko",
-  gender: "female",
-  user: user_2
-)
+# pet_1 = Pet.find_or_create_by(
+#   name: "Spot",
+#   animal: "lizard", 
+#   species: "black and white tegu",
+#   gender: "male",
+#   user: user_1
+# )
+
+# pet_2 = Pet.find_or_create_by(
+#   name: "Pearl",
+#   animal: "lizard",
+#   species: "leopard gecko",
+#   gender: "female",
+#   user: user_2
+# )
